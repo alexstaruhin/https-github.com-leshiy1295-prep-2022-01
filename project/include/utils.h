8d85442a1c6912e092748f52b8183ece734af9b6
kachek-durachek <92647492+kachek-durachek@users.noreply.github.com>
2022-03-20 23:48:07 +0300

    Create utils.h

---
+++ project/include/utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <stddef.h>
+size_t timer_from(unsigned char from);
+// TODO: Implement `power of` function
+int custom_pow(int base, int power);
+
+#endif //UTILS_H
