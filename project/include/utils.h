4d1a3348064c5f5c0ed4aee54306ba168582eddf
kachek-durachek <92647492+kachek-durachek@users.noreply.github.com>
2022-03-21 16:59:38 +0300

    Update utils.h

--- project/include/utils.h
+++ project/include/utils.h
@@ -1,8 +1,10 @@
-#ifndef UTILS_H
-#define UTILS_H
+#ifndef PROJECT_INCLUDE_UTILS_H_
+#define PROJECT_INCLUDE_UTILS_H_
 #include <stddef.h>
-size_t timer_from(unsigned char from);
-// TODO: Implement `power of` function
+#include <stdio.h>
+#include <stdlib.h>
+int timer_from(int from);
+// TODO(Burai202): Implement `power of` function.
 int custom_pow(int base, int power);
 
-#endif //UTILS_H
+#endif  // PROJECT_INCLUDE_UTILS_H_
