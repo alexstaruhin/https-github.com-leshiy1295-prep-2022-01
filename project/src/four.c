15463db17a69a404661626166553d1912e3d91ce
kachek-durachek <92647492+kachek-durachek@users.noreply.github.com>
2022-03-21 16:57:55 +0300

    Update four.c

--- project/src/four.c
+++ project/src/four.c
@@ -1,19 +1,15 @@
-#include <iostream>
 
-#include "four.h"
 
-int prime_num(int number){
-bool res=false;
-for(int i= 1; i < number; i++){
-   if(number % i == 0 && i!=1){
-       res=true;
-       break;
-   }
-}
-if(res==true){
-return 1;
-}
-else{
-return 0;
-}
+#include "primenumber.h"
+
+int prime_number(int number) {
+	int res = 1;
+	for (int i = 1; i < number; i++) {
+		if(number % i == 0 && i != 1) {
+			res = 0;
+			break;
+		}
+	}
+	if(number < 2) {res = 0;}
+	return res;
 }
