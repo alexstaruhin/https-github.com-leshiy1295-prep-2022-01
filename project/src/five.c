946786641c6e152d92e4ca8117f203f39e54434c
kachek-durachek <92647492+kachek-durachek@users.noreply.github.com>
2022-03-21 16:58:51 +0300

    Update five.c

--- project/src/five.c
+++ project/src/five.c
@@ -1,14 +1,20 @@
-#include <iostream>
 
+#include <stdio.h>
 #include "five.h"
 
-int i=1;
 
-void rec(int n){
-       if(i <= n){
-       printf("%d", i);
-       printf("%c", ' ');
-        i++;
-        rec(n);
-        }
-    }
+int i = 1;
+int k = 0;
+void rec(int n) {
+	if (i <= n) {
+		k = 1;
+		printf("%d", i);
+		if(i != n) {printf("%c", ' ');}
+		i++;
+		rec(n); }
+	if(i > n && !k) {
+		printf("%d", i);
+		if(i != n) {printf("%c", ' ');}
+		i--;
+		rec(n); }
+	}
