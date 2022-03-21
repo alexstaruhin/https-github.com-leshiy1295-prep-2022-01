8ac25f31ff8bcc78623cadcea9ef4a4a132cbc3d
kachek-durachek <92647492+kachek-durachek@users.noreply.github.com>
2022-03-21 17:01:27 +0300

    Update and rename five.h to rec.h

--- project/include/five.h
+++
@@ -1,7 +0,0 @@
-#ifndef FIVE_H
-#define FIVE_H
-#include <stddef.h>
-
-void rec(int n);
-
-#endif //UTILS_H
---
+++ project/include/rec.h
@@ -0,0 +1,6 @@
+#ifndef PROJECT_INCLUDE_REC_H_
+#define PROJECT_INCLUDE_REC_H_
+
+void rec(int n);
+
+#endif  // PROJECT_INCLUDE_REC_H_
