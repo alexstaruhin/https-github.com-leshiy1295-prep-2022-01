757bfe79f6ec0060b00372726294a072ee151018
kachek-durachek <92647492+kachek-durachek@users.noreply.github.com>
2022-03-21 17:00:56 +0300

    Update and rename four.h to primenumber.h

--- project/include/four.h
+++
@@ -1,7 +0,0 @@
-#ifndef FOUR_H
-#define FOUR_H
-#include <stddef.h>
-
-int prime_num(int number);
-
-#endif //UTILS_H
---
+++ project/include/primenumber.h
@@ -0,0 +1,7 @@
+#ifndef PROJECT_INCLUDE_PRIMENUMBER_H_
+#define PROJECT_INCLUDE_PRIMENUMBER_H_
+
+int prime_number(int number);
+
+
+#endif  // PROJECT_INCLUDE_PRIMENUMBER_H_
